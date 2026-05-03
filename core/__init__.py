4a2a5e7fc23c159eab9ee9bf69c97fa5bc5ae905
jrojo0153-ux <jrojo0153@gmail.com>
2026-05-03 14:29:21 -0600

    Initialize core module with imports and exports

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+"""Módulo core de EdgeBot-IA-V2"""
+from .analyzer import EdgeBotAnalyzer, AnalysisPick
+from .audit import AuditManager
+
+__all__ = ['EdgeBotAnalyzer', 'AnalysisPick', 'AuditManager']
